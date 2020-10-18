875ec9f45c0693ee49d05b35e71f588928e73df5
unknown <gdmaccabe@hotmail.co.uk>
2020-10-18 14:32:02 +0300

    Add Mockito as Maven Dependency

--- src/test/java/StockManagementTests.java
+++ src/test/java/StockManagementTests.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class StockManagementTests {
 
@@ -8,19 +9,37 @@ public class StockManagementTests {
     public void testCanGetACorrectLocatorCode() {
 
         // create overriding mock version of external 3rd party web service (test stub)
-        ExternalISBNDataService testService = new ExternalISBNDataService() {
+        ExternalISBNDataService testWebService = new ExternalISBNDataService() {
             @Override
             public Book lookup(String isbn) {
                 return new Book(isbn, "Of Mice and Men", "J. Steinbeck");
             }
         };
 
+        ExternalISBNDataService testDatabaseService = new ExternalISBNDataService() {
+            @Override
+            public Book lookup(String isbn) {
+                return null;
+            }
+        };
+
         StockManager stockManager = new StockManager();
-        stockManager.setService(testService);
+        stockManager.setWebService(testWebService);
+        stockManager.setDatabaseService(testDatabaseService);
 
         // Test business logic
         String isbn = "0140177396";
         String locatorCode = stockManager.getLocatorCode(isbn);
         assertEquals("7396J4", locatorCode);
+    }
+
+    @Test
+    public void databaseIsUsedIfDataIsPresent() {
+        fail();
+    }
+
+    @Test
+    public void webServiceIsUsedIfDataIsMissingInDatabase() {
+        fail();
     }
 }
